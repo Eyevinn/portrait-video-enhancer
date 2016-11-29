97f4b231a5df2f86f10263e891b6831bbee64235
mariliab <marilia.bognandi@gmail.com>
2016-11-29 13:23:53 +0100

    Knapp

--- js/main.js
+++ js/main.js
@@ -59,6 +59,7 @@ $(document).ready(function(){
    console.log(isPlaying);
   if (isPlaying) {
     videoPlayer.pause()
+    $(".play-button").innerHTML="Play";
     console.log("if");
     isPlaying = false;
     //playButton.innerHTML='Pause';
@@ -67,6 +68,7 @@ $(document).ready(function(){
   } else {
     console.log("else");
     videoPlayer.play();
+    $(".play-button").innerHTML="Pause";
     isPlaying = true;
    // playButton.innerHTML='Play';
    // playButton.style.backgroundColor = '#C9EAF8 ';
